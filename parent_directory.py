833a22e4801a6e8fcb6c90aa9a6ed603cce2f38a
Muhammad Abu Bakar <92060434+artist80@users.noreply.github.com>
2023-12-09 17:42:22 +0500

    Add files via upload

---
+++ parent_directory.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import os
+
+def parent_directory():
+	# Create a relative path to the parent
+	# of teh current working directory
+	relative_parent = os.path.join(os.getcwd(), "..")
+
+	# Return the absolute path of the parent directory
+	return os.path.abspath(relative_parent)
+
+print(parent_directory())
